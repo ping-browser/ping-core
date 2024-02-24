1dd07a5e97a300c54b23d6ef9ccb4959ab67e949
udaybansal19 <udaybansal19@gmail.com>
2024-02-24 06:38:15 +0530

    Dummy chrome.pkcs11 extension api

--- chromium_src/chrome/common/extensions/permissions/chrome_permission_message_rules.cc
+++ chromium_src/chrome/common/extensions/permissions/chrome_permission_message_rules.cc
@@ -21,6 +21,9 @@ ChromePermissionMessageRule::GetAllRules() {
   rules.push_back({IDS_EXTENSION_PROMPT_WARNING_IPFS_PRIVATE,
                    {APIPermissionID::kIpfsPrivate},
                    {}});
+  rules.push_back({IDS_EXTENSION_PROMPT_WARNING_PKCS11,
+                   {APIPermissionID::kPkcs11},
+                   {}});
   return rules;
 }
 
