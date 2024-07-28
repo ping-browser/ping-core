5f07afccf31a2099011aa7f64cf46172a210f3df
Shubham Kumar <shubham.kumar@rubrik.com>
2024-07-28 14:24:39 +0530

    Fix build failures due to crypto signing on mobile platforms

---
+++ third_party/botan/src/fake_api.cc
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <string.h>
+
+std::string botan_high_level::pkcs11::sign_data(char* module_path, char* pin, char* md_hash) {
+    return "EMPTY_STRING";
+}
+
+std::string botan_high_level::pkcs11::get_cert(char* module_path) {
+    return "EMPTY_STRING";
+}
