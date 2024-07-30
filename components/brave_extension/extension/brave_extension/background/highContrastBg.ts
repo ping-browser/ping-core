9b5bd1804e8d69301f634bc6edd42d2d22a8f16c
nikhil7174 <nikhilkumarsingh7174@gmail.com>
2024-07-30 22:56:14 +0530

    added high contrast in contextMenu

--- components/brave_extension/extension/brave_extension/background/highContrastBg.ts
+++ components/brave_extension/extension/brave_extension/background/highContrastBg.ts
@@ -1,4 +1,9 @@
-//@ts-nocheck
+export const highContrast = () => {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs:any) => {
+    chrome.tabs.sendMessage(tabs[0].id, {action: "toggleContrast"});
+  });
+}
+
 chrome.commands.onCommand.addListener((command) => {
   if (command === "toggle-contrast") {
       chrome.tabs.query({active: true, currentWindow: true}, (tabs:any) => {
