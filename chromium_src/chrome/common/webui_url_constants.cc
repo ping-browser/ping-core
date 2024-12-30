8efa9ca05267f255debeb51fce214b649af53496
MasterK0927 <keshav.agarwal.12325@gmail.com>
2024-12-30 13:08:05 +0530

    sync: migrating webui_url_constants

--- chromium_src/chrome/common/webui_url_constants.cc
+++ chromium_src/chrome/common/webui_url_constants.cc
@@ -4,22 +4,13 @@
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "brave/components/constants/webui_url_constants.h"
-
-#include "brave/components/ipfs/buildflags/buildflags.h"
 #include "chrome/common/webui_url_constants.h"
 
 #define kChromeUIAttributionInternalsHost                                     \
   kChromeUIAttributionInternalsHost, kAdblockHost, kAdblockInternalsHost,     \
       kRewardsPageHost, kRewardsInternalsHost, kWelcomeHost, kWalletPageHost, \
       kTorInternalsHost, kSkusInternalsHost, kBraveCustomNotesHost
-#define kPerformanceSubPage kPerformanceSubPage_UnUsed
-
-#if BUILDFLAG(ENABLE_IPFS_INTERNALS_WEBUI)
-#define kChromeUIBlobInternalsHost kChromeUIBlobInternalsHost, kIPFSWebUIHost
-#endif
-      kTorInternalsHost, kSkusInternalsHost
 
 #include "src/chrome/common/webui_url_constants.cc"
 
 #undef kChromeUIAttributionInternalsHost
-
