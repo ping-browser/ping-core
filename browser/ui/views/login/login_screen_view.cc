da6417a6cb2be39d3f7387638ca80de9a926488d
MasterK0927 <keshav.agarwal.12325@gmail.com>
2025-01-07 17:39:50 +0530

    fix: refactor LoginScreenView to improve UI initialization and callback handling

--- browser/ui/views/login/login_screen_view.cc
+++ browser/ui/views/login/login_screen_view.cc
@@ -4,56 +4,73 @@
  * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "brave/browser/ui/views/login/login_screen_view.h"
-#include "chrome/browser/profiles/profile.h"
+
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/controls/label.h"
 #include "base/logging.h"
+#include "base/task/sequenced_task_runner.h"
 
-LoginScreenView::LoginScreenView(Profile* profile, base::OnceCallback<void()> on_login_success)
-    : on_login_success_(std::move(on_login_success)) {
-  CreateUI(profile);
+LoginScreenView::LoginScreenView(raw_ptr<Profile> profile,
+                               base::OnceCallback<void()> on_login_success)
+    : profile_(profile),
+      username_field_(nullptr),
+      password_field_(nullptr),
+      login_button_(nullptr),
+      on_login_success_(std::move(on_login_success)) {
+  CreateUI();
 }
+
 LoginScreenView::~LoginScreenView() = default;
 
-void LoginScreenView::CreateUI(Profile* profile) {
+void LoginScreenView::CreateUI() {
   SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kVertical, gfx::Insets(10), 10));
 
   auto* username_label = new views::Label(u"Username:");
   AddChildView(username_label);
-
-  usernamefield = new views::Textfield();
-  AddChildView(usernamefield);
-
+  
+  username_field_ = new views::Textfield();
+  AddChildView(username_field_);
+  
   auto* password_label = new views::Label(u"Password:");
   AddChildView(password_label);
-
-  passwordfield = new views::Textfield();
-  passwordfield->SetTextInputType(ui::TextInputType::TEXT_INPUT_TYPE_PASSWORD);
-  AddChildView(passwordfield);
-
-  loginbutton = new views::MdTextButton(
-    base::BindRepeating(&LoginScreenView::OnLoginButtonPressed, base::Unretained(this)),
-    u"Login");
-  AddChildView(loginbutton);
+  
+  password_field_ = new views::Textfield();
+  password_field_->SetTextInputType(ui::TextInputType::TEXT_INPUT_TYPE_PASSWORD);
+  AddChildView(password_field_);
+  
+  login_button_ = new views::MdTextButton(
+      base::BindRepeating(&LoginScreenView::OnLoginButtonPressed,
+                         weak_ptr_factory_.GetWeakPtr()),
+      u"Login");
+  AddChildView(login_button_);
 }
 
 bool LoginScreenView::Accept() {
-  // TODO: Implement a login logic, that will return a callback to the startup browser creator
   return true;
 }
 
-void LoginScreenView::OnLoginButtonPressed() {
-  std::u16string username = usernamefield->GetText();
-  std::u16string password = passwordfield->GetText();
+void LoginScreenView::CloseAndRunCallback() {
+  // Taking ownership of the callback before closing the widget
+  auto callback = std::move(on_login_success_);
+  GetWidget()->CloseNow();
+  
+  // Posting the callback to the task runner to avoid re-entrancy
+  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+      FROM_HERE,
+      base::BindOnce([](base::OnceCallback<void()> callback) {
+        std::move(callback).Run();
+      },
+      std::move(callback)));
+}
 
+void LoginScreenView::OnLoginButtonPressed() {
+  std::u16string username = username_field_->GetText();
+  std::u16string password = password_field_->GetText();
+  
   if (username == u"admin" && password == u"password") {
     LOG(INFO) << "Login successful!";
-    // Closing the login widget
-    GetWidget()->CloseNow();
-    if (on_login_success_)
-     // Notifying the success callback
-      std::move(on_login_success_).Run();
+    CloseAndRunCallback();
   } else {
     LOG(INFO) << "Login failed!";
   }
--- browser/ui/views/login/login_screen_view.h
+++ browser/ui/views/login/login_screen_view.h
@@ -7,6 +7,7 @@
 #define BRAVE_BROWSER_UI_VIEWS_LOGIN_SCREEN_VIEW_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "ui/views/window/dialog_delegate.h"
 #include "ui/views/controls/button/md_text_button.h"
 #include "ui/views/controls/textfield/textfield.h"
@@ -15,24 +16,29 @@
 
 class LoginScreenView : public views::DialogDelegateView {
  public:
-  explicit LoginScreenView(Profile* profile, base::OnceCallback<void()> on_login_success);
+  explicit LoginScreenView(raw_ptr<Profile> profile,
+                         base::OnceCallback<void()> on_login_success);
   ~LoginScreenView() override;
 
   bool Accept() override;
 
  private:
-  void CreateUI(Profile* profile);
+  void CreateUI();
   void OnLoginButtonPressed();
-
-  raw_ptr<views::Textfield> usernamefield;
-  raw_ptr<views::Textfield> passwordfield;
-  raw_ptr<views::MdTextButton> loginbutton;
-
+  void CloseAndRunCallback();
+  
+  raw_ptr<Profile> profile_;
+  
+  raw_ptr<views::Textfield> username_field_;
+  raw_ptr<views::Textfield> password_field_;
+  raw_ptr<views::MdTextButton> login_button_;
+  
   base::OnceCallback<void()> on_login_success_;
-
+  
+  base::WeakPtrFactory<LoginScreenView> weak_ptr_factory_{this};
+  
   LoginScreenView(const LoginScreenView&) = delete;
   LoginScreenView& operator=(const LoginScreenView&) = delete;
 };
-
 
 #endif  // BRAVE_BROWSER_UI_VIEWS_LOGIN_SCREEN_VIEW_H_
